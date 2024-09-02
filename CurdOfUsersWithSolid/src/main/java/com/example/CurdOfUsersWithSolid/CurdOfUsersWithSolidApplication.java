6c1d334eab97120f91fb125f55db4743a3ef75fd
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-09-02 20:02:36 -0300

    first commit

---
+++ CurdOfUsersWithSolid/src/main/java/com/example/CurdOfUsersWithSolid/CurdOfUsersWithSolidApplication.java
@@ -0,0 +1,13 @@
+package com.example.CurdOfUsersWithSolid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurdOfUsersWithSolidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurdOfUsersWithSolidApplication.class, args);
+	}
+
+}
---
+++ CurdOfUsersWithSolid/src/test/java/com/example/CurdOfUsersWithSolid/CurdOfUsersWithSolidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CurdOfUsersWithSolid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurdOfUsersWithSolidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
