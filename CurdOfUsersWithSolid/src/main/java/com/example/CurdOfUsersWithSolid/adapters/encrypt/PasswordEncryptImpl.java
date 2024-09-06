333ad7040415334788e031149be9e4de36a7ef78
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-09-06 20:48:05 -0300

    refactor: refactoring cryptography performance

--- CurdOfUsersWithSolid/src/main/java/com/example/CurdOfUsersWithSolid/adapters/encrypt/PasswordEncryptImpl.java
+++ CurdOfUsersWithSolid/src/main/java/com/example/CurdOfUsersWithSolid/adapters/encrypt/PasswordEncryptImpl.java
@@ -1,24 +1,14 @@
 package com.example.CurdOfUsersWithSolid.adapters.encrypt;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Component;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.Objects;
-
 @Component
 public class PasswordEncryptImpl implements PasswordEncyption{
-    private static final Path path = Paths.get("CurdOfUsersWithSolid", ".env");
-    private static final Dotenv dotEnv = Dotenv.configure()
-            .directory(String.valueOf(path.toAbsolutePath()))
-            .load();
 
     @Override
     public String encrypt(String password) {
-        int saltRounds = Integer.parseInt(Objects.requireNonNull(dotEnv.get("SALT")));
-        String salt = BCrypt.gensalt(saltRounds);
+        String salt = BCrypt.gensalt();
         return BCrypt.hashpw(password, salt);
     }
 
