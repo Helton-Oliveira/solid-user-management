6156ea869ad0ec12141f1d8835e910bb4353f817
Helton-Oliveira <heltonhenriqueoliveira321@gmail.com>
2024-09-06 19:10:12 -0300

    feat: implementing password encryption

---
+++ CurdOfUsersWithSolid/src/main/java/com/example/CurdOfUsersWithSolid/adapters/encrypt/PasswordEncyption.java
@@ -0,0 +1,5 @@
+package com.example.CurdOfUsersWithSolid.adapters.encrypt;
+
+public interface PasswordEncyption {
+    String encrypt(String password);
+}
--- CurdOfUsersWithSolid/src/main/java/com/example/CurdOfUsersWithSolid/core/useCases/RegisterUserUseCaseImpl.java
+++ CurdOfUsersWithSolid/src/main/java/com/example/CurdOfUsersWithSolid/core/useCases/RegisterUserUseCaseImpl.java
@@ -1,5 +1,6 @@
 package com.example.CurdOfUsersWithSolid.core.useCases;
 
+import com.example.CurdOfUsersWithSolid.adapters.encrypt.PasswordEncyption;
 import com.example.CurdOfUsersWithSolid.core.useCases.useCasesAbstractions.RegisterUserUseCase;
 import com.example.CurdOfUsersWithSolid.core.entity.User;
 import com.example.CurdOfUsersWithSolid.core.repository.Connection;
@@ -12,9 +13,12 @@ public class RegisterUserUseCaseImpl implements RegisterUserUseCase {
     @Autowired
     private Connection connection;
 
+    @Autowired
+    private PasswordEncyption encyption;
+
     @Override
     public User execute(String name, String email, String password, String cpf, String phone) {
-        var entity = new User(name, email, password, cpf, phone);
+        var entity = new User(name, email, encyption.encrypt(password), cpf, phone);
         return connection.saveUser(entity);
     }
 }
